c2b2b2b2422b501a1c7b17696f182d23d65936ba
wuliya <3369906077@qq.com>
2026-05-28 11:07:34 +0800

    chore(puniyu_adapter_api): 添加对临时群聊消息的支持
    - 引入anyhow依赖用于错误处理
    - 在OneBotAdapterApi中重新排列方法顺序以提高可读性
    - 扩展send_message实现以支持GroupTemp类型的联系人
    - 将未支持的联系人类型错误从panic改为返回anyhow错误

--- crates/puniyu_adapter_api/src/onebot.rs
+++ crates/puniyu_adapter_api/src/onebot.rs
@@ -1,45 +1,43 @@
 use async_trait::async_trait;
 use puniyu_adapter_types::SendMsgType;
 use puniyu_contact::{Contact, ContactType};
-use puniyu_message::Message;
 use puniyu_error::Result;
+use puniyu_message::Message;
 
 use crate::AdapterApi;
+use anyhow::anyhow;
 
 #[async_trait]
 pub trait OneBotAdapterApi: Send + Sync {
+    /// 发送私聊消息
+	async fn send_private_msg(&self, user_id: u64, message: &Message) -> Result<SendMsgType>;
 
-    async fn send_private_msg(
-        &self,
-        user_id: u64,
-        message: &Message,
-    ) -> Result<SendMsgType>;
-
-    async fn send_group_msg(
-        &self,
-        group_id: u64,
-        message: &Message,
-    ) -> Result<SendMsgType>;
+    /// 发送群消息
+	async fn send_group_msg(&self, group_id: u64, message: &Message) -> Result<SendMsgType>;
 }
 
 #[async_trait]
 impl<T: OneBotAdapterApi> AdapterApi for T {
-    #[allow(clippy::unwrap_used)]
-    async fn send_message(
-        &self,
-        contact: &ContactType<'_>,
-        message: &Message,
-    ) -> Result<SendMsgType> {
-        match contact {
-            ContactType::Friend(contact) => {
-                self.send_private_msg(contact.peer().parse::<u64>()?, message).await
-            }
-            ContactType::Group(contact) => {
-                self.send_group_msg(contact.peer().parse::<u64>()?, message).await
-            }
-            _ => unimplemented!("不支持的 ContactType: {:?}", contact),
-        }
-    }
+	async fn send_message(
+		&self,
+		contact: &ContactType<'_>,
+		message: &Message,
+	) -> Result<SendMsgType> {
+		match contact {
+			ContactType::Friend(contact) => {
+				self.send_private_msg(contact.peer().parse::<u64>()?, message).await
+			}
+			ContactType::Group(contact) => {
+				self.send_group_msg(contact.peer().parse::<u64>()?, message).await
+			}
+			ContactType::GroupTemp(concat) => {
+				self.send_private_msg(concat.peer().parse::<u64>()?, message).await
+			}
+			_ => Err(anyhow!("unsupported contact type: {:?}", contact).into()),
+		}
+	}
 
-    fn as_onebot(&self) -> Option<&dyn OneBotAdapterApi> { Some(self) }
+	fn as_onebot(&self) -> Option<&dyn OneBotAdapterApi> {
+		Some(self)
+	}
 }
